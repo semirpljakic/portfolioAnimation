94b499cd2564f4dfbf707345938b345d9582dcaa
Semir Pljakic <pljakicsemir@gmail.com>
2026-03-23 15:23:48 +0100

    newAnimations2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
               </a>
             </p>
             <h4>Education</h4>
-            <p>BSc in Computer Science</p>
+            <p>Elegant group</p>
           </div>
           <div className="contact-box">
             <h4>Social</h4>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          RC
+          SP
         </a>
         <a
           href="mailto:pljakicsemir@gmail.com"
