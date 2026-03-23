b46023c4a87943de8246b5d2b2adf43786ba74ca
Semir Pljakic <pljakicsemir@gmail.com>
2026-03-23 16:17:42 +0100

    New animation6

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,10 @@
+declare module "gsap-trial/SplitText" {
+  export class SplitText {
+    constructor(target: any, vars?: any);
+    split: any;
+    revert: () => void;
+    chars: any[];
+    lines: any[];
+    words: any[];
+  }
+}
